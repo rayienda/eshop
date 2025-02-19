30ed8af5a9be1b9435db2b1c1d71c39bfe3e8527
rayienda <rayienda.hasmaradana@ui.ac.id>
2025-02-19 15:28:17 +0700

    fix quality issue

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
